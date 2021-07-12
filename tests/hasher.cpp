70976e1eed33acc16ee9166eb8a8ecfb01e4f3fe
Murat Keceli <keceli@gmail.com>
2021-07-12 17:07:15 -0500

    Added more hasher tests

--- tests/hasher.cpp
+++ tests/hasher.cpp
@@ -2,6 +2,7 @@
 #include <catch2/catch.hpp>
 #include <functional>
 #include <iostream>
+#include <limits>
 
 using namespace pz;
 TEST_CASE("Hashing with BPHash") {
@@ -29,19 +30,32 @@ TEST_CASE("Hashing with BPHash") {
     REQUIRE(hash_objects(v) == hash_to_string(make_hash(v)));
     REQUIRE(hash_objects(v) == hash_objects(std::vector<int>{1, 2, 3}));
     REQUIRE_FALSE(hash_objects(rv) == hash_objects(std::vector<int>{3, 2, 1}));
-    REQUIRE_FALSE(hash_objects(double(i)) == hash_objects(float(i)));
-// BPHash adds typeid to the hash when BPHASH_USE_TYPEID is defined.
+    // BPHash adds typeid to the hash when BPHASH_USE_TYPEID is defined.
+    SECTION("BPHASH_USE_TYPEID") {
 #ifdef BPHASH_USE_TYPEID
-    std::cout << "BPHash uses typeid for hashes. \n";
-    REQUIRE_FALSE(hash_objects(rv) == hash_objects(v));
-    REQUIRE_FALSE(hash_objects(rs) == hash_objects(i, v));
-    REQUIRE_FALSE(hash_objects(sa) == hash_objects(sb));
-    REQUIRE_FALSE(hash_objects(sa0) == hash_objects(sb0));
+        std::cout << "BPHash uses typeid for hashes. \n";
+        REQUIRE_FALSE(hash_objects(rv) == hash_objects(v));
+        REQUIRE_FALSE(hash_objects(rs) == hash_objects(i, v));
+        REQUIRE_FALSE(hash_objects(sa) == hash_objects(sb));
+        REQUIRE_FALSE(hash_objects(sa0) == hash_objects(sb0));
 #else
-    REQUIRE(hash_objects(rv) == hash_objects(v));
-    REQUIRE(hash_objects(rs) == hash_objects(i, v));
-    REQUIRE(hash_objects(sa) == hash_objects(sb));
-    // Hash of empty objects might still differ even if typeid is not used.
-    // REQUIRE(hash_objects(sa0) == hash_objects(sb0));
+        REQUIRE(hash_objects(rv) == hash_objects(v));
+        REQUIRE(hash_objects(rs) == hash_objects(i, v));
+        REQUIRE(hash_objects(sa) == hash_objects(sb));
+        // Hash of empty objects might still differ even if typeid is not used.
+        // REQUIRE(hash_objects(sa0) == hash_objects(sb0));
 #endif
+    }
+    SECTION("Different types, same value") {
+        REQUIRE_FALSE(hash_objects(double(1.)) == hash_objects(float(1.)));
+        REQUIRE_FALSE(hash_objects(short(1)) == hash_objects(int(1.)));
+    }
+    SECTION("Epsilon difference") {
+        double depsilon = std::numeric_limits<double>::epsilon();
+        float fepsilon  = std::numeric_limits<float>::epsilon();
+        REQUIRE_FALSE(hash_objects(double(1.)) ==
+                      hash_objects(double(1. + depsilon)));
+        REQUIRE_FALSE(hash_objects(float(1.)) ==
+                      hash_objects(float(1. + fepsilon)));
+    }
 }

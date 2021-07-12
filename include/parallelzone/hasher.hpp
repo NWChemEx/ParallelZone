202811edc855bcc140e69b56afd33fe32e612eaa
Murat Keceli <keceli@gmail.com>
2021-07-12 16:39:18 -0500

    Add documentation for hasher.hpp

--- include/parallelzone/hasher.hpp
+++ include/parallelzone/hasher.hpp
@@ -1,5 +1,10 @@
 #pragma once
-
+/** @file hasher.hpp
+ *
+ * This header provides the hashing API.
+ * Currently, it is based on BPHash library.
+ *
+ */
 #include <bphash/Hasher.hpp>
 #include <bphash/types/All.hpp>
 #include <functional>
@@ -12,10 +17,26 @@ using HashType  = bphash::HashType;
 
 inline auto make_hasher() { return Hasher(HashType::Hash128); }
 
+/*! \brief Return a string representation of a hash
+ *
+ * The string in lower case hex representation
+ * with the length of 2*number_of_bits/8 characters.
+ *
+ * \param [in] hash The hash
+ * \return A string representing the hash
+ */
 inline std::string hash_to_string(const HashValue& hash) {
     return bphash::hash_to_string(hash);
 }
 
+/*! \brief Generates a hash string in a single call
+ *
+ * This can be used to easily obtain the hash string for several
+ * objects at once without creating a Hasher object manually.
+ *
+ * \param [in] args Objects to hash
+ * \return Hash string of the given object(s)
+ */
 template<typename... Args>
 auto hash_objects(Args&&... args) {
     auto h = make_hasher();
@@ -23,6 +44,14 @@ auto hash_objects(Args&&... args) {
     return bphash::hash_to_string(h.finalize());
 }
 
+/*! \brief Generates a hash in a single call
+ *
+ * This can be used to easily obtain the hash of several objects at once
+ * without creating a Hasher object manually.
+ *
+ * \param [in] args Objects to hash
+ * \return 128 bit hash of the given object(s)
+ */
 template<typename... Args>
 auto make_hash(Args&&... args) {
     auto h = make_hasher();

3f27da20cfa245088be7bd39ca84d284779f3474
Abhishek Bagusetty <abagusetty@anl.gov>
2021-10-14 02:43:06 +0000

    [PAPI] add papi interface for nvidia and amd

---
+++ src/parallelzone/Exception.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace pz {
+
+  class pz_exception : public std::runtime_error {
+    std::string msg;
+  public:
+    pz_exception(const std::string &arg, const char *file, int line) :
+      std::runtime_error(arg) {
+      std::ostringstream o;
+      o << file << ":" << line << ": " << arg;
+      msg = o.str();
+    }
+    
+    ~pz_exception() throw() {}
+
+    const char *what() const throw() {
+      return msg.c_str();
+    }
+  };
+
+}
---
+++ src/parallelzone/Parallel.hpp
@@ -0,0 +1,35 @@
+#ifndef _Parallel_HPP_
+#define _Parallel_HPP_
+
+#include "mpi.h"
+
+class Parallel {
+
+  int npi[3],taskidi[3];
+  int *reqcnt;
+  int *procNd;
+  //MPI::Intracomm comm_i[3];
+  //MPI::Group     group_i[3];
+  //MPI::Request  **request;
+  MPI_Comm      comm_world;
+  MPI_Comm      comm_i[3];
+  MPI_Group     group_i[3];
+  MPI_Request  **request;
+  MPI_Status   **statuses;
+
+public:
+  int dim;
+
+  /* Constructors */
+  Parallel(MPI_Comm mpi_comm);
+
+  ~Parallel();
+
+  /* 2d proc geom constructor */
+  void init2d(const int, const int);
+
+  int is_master() { return (taskidi[0]==MASTER); }
+
+}
+
+#endif

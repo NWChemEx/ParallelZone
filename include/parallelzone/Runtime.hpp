061704686052571c82a5db0bf177f2584be4e2d4
Abhishek Bagusetty <abagusetty@anl.gov>
2022-03-07 18:56:50 +0000

    fix docs, unit tests for Runtime

--- include/parallelzone/Runtime.hpp
+++ include/parallelzone/Runtime.hpp
@@ -10,8 +10,39 @@ namespace parallelzone {
 
 class Runtime {
 public:
+    /**
+     * @brief Default constructor
+     * @details Runtime instance initializes MADness runtime, sets partitions
+     * to 1.
+     */
+    Runtime();
+
+    /**
+     * @brief constructor
+     * @details Runtime instance initializes MADness runtime, sets partitions
+     * to 1.
+     */
+    Runtime(int argc, char** argv);
+
+    /**
+     * @brief constructor
+     * @details Starts MADness using a MPI Communicator. Runtime instance
+     * doesn't explicitly start MADness
+     */
+    Runtime(const MPI_Comm& comm);
+
+    /**
+     * @brief constructor
+     * @details Starts MADness using a SafeMPI Communicator. Runtime instance
+     * doesn't explicitly start MADness
+     */
+    Runtime(const SafeMPI::Intracomm& comm);
+
     /// @return MPI Communicator, associated with MADWorld
-    MPI_Comm& mpi_comm() { return madness_world.mpi.comm().Get_mpi_comm(); }
+    MPI_Comm& mpi_comm() { return mad_world.mpi.comm().Get_mpi_comm(); }
+
+    /// @return MADWorld, ref handle to madness::World
+    madness::World& madness_world() { return mad_world; }
 
     /// @return int, number of partitions; Defaults to 1
     constexpr int get_num_partitions() const noexcept { return num_partitions; }
@@ -20,12 +51,11 @@ public:
     /// initialized state
     constexpr bool is_initialized() const noexcept { return initialized; }
 
-private:
-    Runtime();
-    Runtime(int argc, char** argv);
-    Runtime(const MPI_Comm& comm);
-    Runtime(const SafeMPI::Intracomm& comm);
-
+    /**
+     * @brief destructor
+     * @details Destructs the MADness runtime appropriately, if Runtime is
+     * initialized
+     */
     ~Runtime();
 
     Runtime(const Runtime&)            = delete;
@@ -33,9 +63,10 @@ private:
     Runtime(Runtime&&)                 = delete;
     Runtime& operator=(Runtime&&)      = delete;
 
+private:
     bool initialized;
     int num_partitions;
-    madness::World& madness_world;
+    madness::World& mad_world;
 };
 
 } // End namespace parallelzone
